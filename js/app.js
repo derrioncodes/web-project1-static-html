be5b0dba910807dcdc4b4093366b5ba873ff6868
Malik Hall <malikhall247@gmail.com>
2025-08-06 19:49:31 -0600

    added product description for single product

--- js/app.js
+++ js/app.js
@@ -226,6 +226,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+// ACCORDIAN
+document.addEventListener("DOMContentLoaded", function () {
+  const headers = document.querySelectorAll(".accordion-header");
+
+  headers.forEach(header => {
+    header.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+
+      // Toggle active class
+      this.classList.toggle("active");
+
+      // Slide toggle effect
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  });
+});
+
+
+
+
+
 
 
 
